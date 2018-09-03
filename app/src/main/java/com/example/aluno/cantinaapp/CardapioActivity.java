fe2931124799a881dc8df775a14cab54ab28c571
Euller <eullerifpr@gmail.com>
2018-09-03 10:05:16 -0300

    Cardapio

--- app/src/main/java/com/example/aluno/cantinaapp/CardapioActivity.java
+++ app/src/main/java/com/example/aluno/cantinaapp/CardapioActivity.java
@@ -2,12 +2,44 @@ package com.example.aluno.cantinaapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+
+import java.text.DecimalFormat;
 
 public class CardapioActivity extends AppCompatActivity {
+     private Button buttonSalgado;
+     private Button buttonRefrigerante;
+     private Button buttonPacoca;
+     private Button buttonBolo;
+     private Button buttonCafe;
+
+     private Button buttonConcluir;
+
+     private double salgado;
+     private double refrigerante;
+     private double pacoca;
+     private double bolo;
+     private double cafe;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cardapio);
+
+
+
+
+        salgado = 3.80;
+        refrigerante = 1.50;
+        pacoca = 0.75;
+        bolo = 2.00;
+        cafe = 1.00;
+
+        buttonSalgado = (Button) findViewById(R.id.buttonSalgado);
+        buttonRefrigerante = (Button) findViewById(R.id.buttonRefrigerante);
+        buttonPacoca = (Button) findViewById(R.id.buttonPacoca);
+        buttonBolo = (Button) findViewById(R.id.buttonBolo);
+        buttonCafe = (Button) findViewById(R.id.buttonCafe);
+
     }
 }
