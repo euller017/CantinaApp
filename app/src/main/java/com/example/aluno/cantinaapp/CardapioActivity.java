c2c9d6e3e5e1468f4d94b66233e6c92c2b90c64f
euller <eullerifpr@gmail.com>
2018-09-03 15:03:06 -0300

    click cardapio

--- app/src/main/java/com/example/aluno/cantinaapp/CardapioActivity.java
+++ app/src/main/java/com/example/aluno/cantinaapp/CardapioActivity.java
@@ -2,31 +2,43 @@ package com.example.aluno.cantinaapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import java.text.DecimalFormat;
 
 public class CardapioActivity extends AppCompatActivity {
-     private Button buttonSalgado;
-     private Button buttonRefrigerante;
-     private Button buttonPacoca;
-     private Button buttonBolo;
-     private Button buttonCafe;
+    private Button buttonSalgado;
+    private TextView QntdSalgado;
+    private Button buttonRefrigerante;
+    private TextView QntdRefri;
+    private Button buttonPacoca;
+    private TextView QntdPacoca;
+    private Button buttonBolo;
+    private TextView QntdBolo;
+    private Button buttonCafe;
+    private TextView QntdCafe;
 
-     private Button buttonConcluir;
+    private Button buttonConcluir;
 
-     private double salgado;
-     private double refrigerante;
-     private double pacoca;
-     private double bolo;
-     private double cafe;
+    private double salgado;
+    private double refrigerante;
+    private double pacoca;
+    private double bolo;
+    private double cafe;
+
+    private String formato;
+    private DecimalFormat decimalFormat;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cardapio);
 
-
+        formato = "##.#";
+        decimalFormat = new DecimalFormat(formato);
 
 
         salgado = 3.80;
@@ -35,11 +47,62 @@ public class CardapioActivity extends AppCompatActivity {
         bolo = 2.00;
         cafe = 1.00;
 
+
+
         buttonSalgado = (Button) findViewById(R.id.buttonSalgado);
+        QntdSalgado = (TextView) findViewById(R.id.textViewQntdSalgado);
         buttonRefrigerante = (Button) findViewById(R.id.buttonRefrigerante);
+        QntdRefri = (TextView) findViewById(R.id.textViewQntdRefri);
         buttonPacoca = (Button) findViewById(R.id.buttonPacoca);
+        QntdPacoca = (TextView) findViewById(R.id.textViewQntdPacoca);
         buttonBolo = (Button) findViewById(R.id.buttonBolo);
+        QntdBolo = (TextView) findViewById(R.id.textViewQntdBolo);
         buttonCafe = (Button) findViewById(R.id.buttonCafe);
+        QntdCafe = (TextView) findViewById(R.id.textViewQntdCafe);
+
+        buttonSalgado.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+
+        });
+        buttonRefrigerante.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
+        buttonBolo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        buttonBolo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        buttonCafe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        private void atualizarSalgados(String tipo) {
+
+            // Não precisa de chave porque é uma instrução só
+            if (tipo.equals("Salgado"))
+
+
+        }
+
 
     }
 }
+
+
+
+
